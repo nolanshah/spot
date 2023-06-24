73fda662be5365affd4d0866f274d8d2441c72fe
Nolan Shah <nolan@0x9b.com>
2023-06-24 14:03:42 -0500

    Add basic cli and testing stuff

--- Makefile
+++ Makefile
@@ -1,4 +1,12 @@
 
+format:
+	gofmt -s -w ./
+
+build: 
+	go build
 
 release:
 	goreleaser release --snapshot --clean
+
+test-local:
+	go run main --input test/input --output test/output
--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
 func main() {
-  println("Ba dum, tss!")
+	app := &cli.App{
+		Name:  "easy-static",
+		Usage: "build static websites from a bunch of unstructured docs",
+		Flags: []cli.Flag{
+			&cli.PathFlag{
+				Name:     "input",
+				Usage:    "input directory",
+				Required: true,
+			},
+			&cli.PathFlag{
+				Name:     "config",
+				Usage:    "config directory",
+				Required: false,
+			},
+			&cli.PathFlag{
+				Name:     "output",
+				Usage:    "output directory",
+				Required: true,
+			},
+			&cli.BoolFlag{
+				Name:   "watch",
+				Value:  false,
+				Hidden: true,
+			},
+		},
+		Action: func(cCtx *cli.Context) error {
+			fmt.Println("boom! I say!")
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
