6536bf0aa939cdb49f8fc7a9e19ce02aedcfa9c1
Nolan Shah <nolan@0x9b.com>
2023-07-23 01:15:43 -0500

    get template dependencies working

--- internal/application/template.go
+++ internal/application/template.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/rs/zerolog/log"
@@ -14,26 +15,48 @@ func hasPrefix(prefix string, s string) bool {
 	return strings.HasPrefix(s, prefix)
 }
 
+func loadTemplates(baseTemplateDirPath string, templateName string) *template.Template {
+	var paths []string
+	err := filepath.Walk(baseTemplateDirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			paths = append(paths, path)
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatal().Err(err).Str("baseTemplateDirPath", baseTemplateDirPath).Msg("Failed to collect templates from base dir.")
+	}
+
+	log.Trace().Any("paths", paths).Msg("Template paths.")
+
+	// Parse the template file
+	tmpl, err := template.New(templateName).Funcs(template.FuncMap{
+		"HasPrefix": hasPrefix,
+	}).ParseFiles(paths...)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to parse templates.")
+	}
+
+	return tmpl
+}
+
 func ApplyTemplateToFile(tData TData) error {
 	templatePath := tData.Page.TemplatePath
 	contentHtmlPath := tData.Page.DestinationPath
 
-	// Parse the template file
-	tmpl, err := template.New(path.Base(templatePath)).Funcs(template.FuncMap{
-		"HasPrefix": hasPrefix,
-	}).ParseFiles(templatePath)
-	if err != nil {
-		log.Error().Err(err).Msg("Failed to parse template.")
-	}
+	tmpl := loadTemplates(path.Dir(templatePath), path.Base(templatePath))
 
 	// Create a buffer to hold the rendered output
-	output := &os.File{}
-	if output, err = os.Create(contentHtmlPath); err != nil {
+	output, err := os.Create(contentHtmlPath)
+	if err != nil {
 		log.Error().Err(err).Msg("Failed to create output file.")
 	}
 	defer output.Close()
 
-	log.Trace().Str("templatePath", templatePath).Any("data", tData).Msg("Attempting to apply template with the following data.")
+	log.Trace().Str("templatePath", path.Base(templatePath)).Any("data", tData).Msg("Attempting to apply template with the following data.")
 
 	// Apply the template to the contents and write the output to the file
 	if err := tmpl.Execute(output, tData); err != nil {
@@ -41,7 +64,7 @@ func ApplyTemplateToFile(tData TData) error {
 		return err
 	}
 
-	log.Trace().Str("template", templatePath).Str("file", contentHtmlPath).Msg("Successfully applied template to file.")
+	log.Trace().Str("template", path.Base(templatePath)).Str("file", contentHtmlPath).Msg("Successfully applied template to file.")
 
 	return nil
 }
