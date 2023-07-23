ca8aa2239bac00ba6823113e2e3007ebae091c9e
Nolan Shah <nolan@0x9b.com>
2023-07-22 21:57:54 -0500

    support most basic form of path list in template rendering

--- internal/application/template.go
+++ internal/application/template.go
@@ -4,9 +4,16 @@ import (
 	"html/template"
 	"io/ioutil"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/rs/zerolog/log"
 )
+
+func hasPrefix(prefix string, s string) bool {
+	log.Logger.Info().Str("prefix", prefix).Str("string", s).Msg("Called hasPrefix")
+	return strings.HasPrefix(s, prefix)
+}
 
 func ApplyTemplateToFile(contentHtmlPath string, templatePath string, pages *[]string) error {
 	// Read the contents of the file
@@ -16,7 +23,9 @@ func ApplyTemplateToFile(contentHtmlPath string, templatePath string, pages *[]s
 	}
 
 	// Parse the template file
-	tmpl, err := template.ParseFiles(templatePath)
+	tmpl, err := template.New(path.Base(templatePath)).Funcs(template.FuncMap{
+		"HasPrefix": hasPrefix,
+	}).ParseFiles(templatePath)
 	if err != nil {
 		log.Error().Err(err).Msg("Failed to parse template")
 	}
@@ -35,6 +44,8 @@ func ApplyTemplateToFile(contentHtmlPath string, templatePath string, pages *[]s
 		Contents: template.HTML(contents),
 		Pages:    *pages,
 	}
+
+	log.Info().Str("templatePath", templatePath).Any("data", data).Msg("Attempting to apply template with the following data")
 
 	// Apply the template to the contents and write the output to the file
 	if err := tmpl.Execute(output, data); err != nil {
